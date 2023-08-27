9758d658426dc745248dc238c6d17ca374d5be00
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-08-27 23:54:28 +0700

    1 file modified, 1 added

---
+++ components/Nav.vue
@@ -0,0 +1,8 @@
+<template>
+
+<div class="diygw-title diygw-top solid-bottom" style="color: white; background: #ff32ad">
+					<div class="title flex1 text-center">หน้าหลัก</div>
+				</div>
+
+
+</template>
