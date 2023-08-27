493f92a69f5ea768d02291712abf8ec9e273548d
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-08-28 00:00:05 +0700

    1 binaries uploaded

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   
     <div class="flex flex-wrap diygw-col-0 items-stretch flex9-clz">
 					<div class="diygw-col-0 text3-clz">กิจกรรมพิเศษ</div>
-					<img src="./static/sd.png" class="image-size diygw-image diygw-col-0 image-clz" mode="heightFix" />
+					<img src="/static/sd.png" class="image-size diygw-image diygw-col-0 image-clz" mode="heightFix" />
 				</div>
 
 
