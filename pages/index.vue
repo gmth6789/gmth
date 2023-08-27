4b72f00a20af996ad8fe8691990d8e52591251ff
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-08-28 00:03:02 +0700

    1 file modified

--- pages/index.vue
+++ pages/index.vue
@@ -9,27 +9,27 @@
         <div class="flex flex-wrap diygw-col-24 flex-clz">
 					<div class="flex flex-wrap diygw-col-6 flex-direction-column items-center">
 						<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
-							<img src="./static/icon1_xsqg.png" class="image2-size diygw-image diygw-col-0 image2-clz" mode="widthFix" />
+							<img src="/static/icon1_sz1.png" class="image2-size diygw-image diygw-col-0 image2-clz" mode="widthFix" />
 							<div class="diygw-text-line1 diygw-col-0 animate__animated animate__headShake animate__infinite text9-clz">免费</div>
 						</div>
 						<div class="diygw-text-line1 diygw-col-0 text8-clz">购买主题</div>
 					</div>
 					<div class="flex flex-wrap diygw-col-6 flex-direction-column items-center">
 						<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
-							<img src="./static/icon1_xsqg.png" class="image3-size diygw-image diygw-col-0 image3-clz" mode="widthFix" />
+							<img src="/static/icon1_sz1.png" class="image3-size diygw-image diygw-col-0 image3-clz" mode="widthFix" />
 						</div>
 						<div class="diygw-text-line1 diygw-col-0 text7-clz">限时抢购</div>
 					</div>
 					<div class="flex flex-wrap diygw-col-6 flex-direction-column items-center">
 						<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
-							<img src="./static/icon1_bq.png" class="image1-size diygw-image diygw-col-0 image1-clz" mode="widthFix" />
+							<img src="/static/icon1_sz1.png" class="image1-size diygw-image diygw-col-0 image1-clz" mode="widthFix" />
 							<div class="diygw-text-line1 diygw-col-0 animate__animated animate__headShake animate__infinite text-clz">免费</div>
 						</div>
 						<div class="diygw-text-line1 diygw-col-0 text1-clz">优惠券</div>
 					</div>
 					<div class="flex flex-wrap diygw-col-6 flex-direction-column items-center">
 						<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
-							<img src="./static/icon1_kf.png" class="image4-size diygw-image diygw-col-0 image4-clz" mode="widthFix" />
+							<img src="/static/icon1_sz1.png" class="image4-size diygw-image diygw-col-0 image4-clz" mode="widthFix" />
 						</div>
 						<div class="diygw-text-line1 diygw-col-0 text5-clz">在线客服</div>
 					</div>
