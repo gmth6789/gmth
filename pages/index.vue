46180d4e2f138b1fb11e0d8ac3c20dbef45752dc
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-08-27 23:57:21 +0700

    1 file modified

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="container">
-    
-  </div>
+  
+    <div class="flex flex-wrap diygw-col-0 items-stretch flex9-clz">
+					<div class="diygw-col-0 text3-clz">กิจกรรมพิเศษ</div>
+					<img src="./static/sd.png" class="image-size diygw-image diygw-col-0 image-clz" mode="heightFix" />
+				</div>
+ 
 </template>
 
 <script>
